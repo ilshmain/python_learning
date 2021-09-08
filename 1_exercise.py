bcd350e0fbc83f892e9f2fb345005c48f3c3d6d4
ilshmain <alesah14>
2021-09-08 16:58:10 +0300

    ex_1

--- 1_exercise.py
+++ 1_exercise.py
@@ -1,2 +1,29 @@
-print('hi')
-print('what you name')
+#Задание_1
+"""
+a = int(input('Введите первое целое число: '))
+b = int(input('Введите второе целое число: '))
+c = int(input('Введите второе целое число: '))
+if (a <= b) & (a <= c):
+    print(a)
+    print(type(a))
+elif (b <= a) & (b <= c):
+    print(b)
+    print(type(b))
+else:
+    print(c)
+    print(type(c))
+"""
+#Задание_2
+
+a = int(input('Введите первое целое число: '))
+b = int(input('Введите второе целое число: '))
+c = int(input('Введите второе целое число: '))
+d = 0
+if (a == b) & (a == c):
+    d += 3
+    print(d)
+elif (a == b) | (a == c):
+    d += 2
+    print(d)
+else:
+    print(d)
